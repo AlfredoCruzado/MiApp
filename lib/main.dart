0f5772006fbb484085441972a6d6cdbc8c8e28d5
AlfredoCruzado <64969169+AlfredoCruzado@users.noreply.github.com>
2022-10-17 16:32:53 -0500

    cummit
    columnas , listview , boton

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,34 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("titulo"),
-      ),
-      body: Center(
-        child: Text("contenido"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("titulo"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: Text(
+                "titulo1",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Text(
+              "titulo2",
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              child: RaisedButton(
+                  child: Text("hola"),
+                  child:
+                  onPressed: () {
+                    var perro = DateTime.now()
+                    print(perro);
+                  }),
+            )
+          ],
+        ));
   }
 }
